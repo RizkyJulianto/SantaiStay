840f5750fa1c9e1cf1a52af08832eb0750277f0e
Juliantomaga <juliantorizky01@gmail.com>
2024-09-17 20:20:20 +0700

    membuat card kamar

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ hamburgerMenu.addEventListener('click', function() {
         navLink.style.display = 'none';
     }
 });
+
+// Membatasi paragraf pada kamar-deskripsi 
+
+const paragraf = document.querySelectorAll('.kamar-deskripsi p');
+
+// Mmebuat fungsi untuk memotong teks
+function potongTeks(elemen, batasKarakter) {
+
+//   Menampung paragraf yang belum dipotong kedalam variable teksAsli
+  const teksAsli = elemen.textContent;
+// pengecekan apakah paragraf yang belum dipotong memiliki panjang lebih dari batas karakter 
+  if (teksAsli.length > batasKarakter) {
+    const teksPotong = teksAsli.substring(0, batasKarakter) + '...';
+    elemen.textContent = teksPotong;
+  }
+}
+
+//Lakukan ke semua paragraf yang belum dipotong 
+paragraf.forEach(paragraf => {
+    potongTeks(paragraf, 100);
+});
